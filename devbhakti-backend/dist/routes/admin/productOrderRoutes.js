1e3b5fa76338fd4936cee3cc6b34e942873f5a3b
ShivpalBellway <shivpal@bellwayinfotech.com>
2026-02-05 14:40:20 +0530

    chore: stop tracking build files (dist folder)

--- devbhakti-backend/dist/routes/admin/productOrderRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const productOrderManagementController_1 = require("../../controllers/admin/productOrderManagementController");
-const router = (0, express_1.Router)();
-router.get("/", productOrderManagementController_1.getAllOrdersAdmin);
-router.patch("/sub-order/:subOrderId", productOrderManagementController_1.updateSubOrderStatusAdmin);
-exports.default = router;
--- devbhakti-backend/dist/routes/temple_admin/productOrderRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const templeOrderController_1 = require("../../controllers/temple_admin/templeOrderController");
-const router = (0, express_1.Router)();
-router.get("/:templeId", templeOrderController_1.getTempleOrders);
-router.patch("/sub-order/:subOrderId", templeOrderController_1.updateTempleOrderStatus);
-exports.default = router;
